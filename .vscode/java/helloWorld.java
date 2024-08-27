0181042bb94395cd1f900ba4198525194a75189b
marcos eduardo <165714407+MEduardoCarvalho@users.noreply.github.com>
2024-08-27 00:15:20 +0000

    add helloWorld.java

---
+++ .vscode/java/helloWorld.java
@@ -0,0 +1,9 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("ola, Mundo!");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
+}
