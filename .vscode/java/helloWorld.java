1ddb8ea9f8925733e60232406e64638bc848c13f
marcos eduardo <165714407+MEduardoCarvalho@users.noreply.github.com>
2024-09-11 09:25:41 +0000

    variaveis.js

--- .vscode/java/helloWorld.java
+++ .vscode/java/helloWorld.java
@@ -1,5 +1,6 @@
 public class helloWorld {
     public static void main(String[] args) {
+        // assim que faz comentario //
         System.out.println("ola, Mundo!");
         System.out.println(2+2);
         System.out.println(2-2);
---
+++ .vscode/java/variaveis.java
@@ -0,0 +1,12 @@
+public class variaveis{
+    public static void main(String[] args) {
+        String cachorro = "nina";
+        int idade = 5;
+        double peso = 15.800;
+        boolean vdd = false;
+
+        System.out.println(cachorro);
+        System.out.println(idade);
+        System.out.println(peso);
+        System.out.println(vdd);
+}}        
