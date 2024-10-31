7fa4646c7873fd336fd3c9157f67485a84a7b001
marcos eduardo <165714407+MEduardoCarvalho@users.noreply.github.com>
2024-10-31 10:03:30 +0000

    novo projeto

---
+++ .vscode/java/funcao.java
@@ -0,0 +1,19 @@
+public class funcao {
+public static void main(String[] args) {
+    soma(10,32);
+    soma(12,34);
+    soma(1,30);
+    soma(140,88);
+    soma(102,38);
+    soma(101,7);
+    soma(102,37);
+    soma(1340,34);
+    soma(103,35);
+
+}
+static void soma(int x, int y) {
+  System.out.println(x + y);
+}
+
+
+}
--- .vscode/java/variaveis.java
+++ .vscode/java/variaveis.java
@@ -1,12 +1,15 @@
 public class variaveis{
     public static void main(String[] args) {
-        String cachorro = "nina";
-        int idade = 5;
-        double peso = 15.800;
-        boolean vdd = false;
+        String cachorro = "nina"; // texto
+        int idade = 5; // numeros inteiros
+        double peso = 15.800; // numeros quebrados
+        boolean trueOU = false; // apesa True ou false
+        var universal = "aceita qualquer coisa";
 
         System.out.println(cachorro);
         System.out.println(idade);
         System.out.println(peso);
-        System.out.println(vdd);
-}}        
+        System.out.println(trueOU);
+        System.out.println(universal);
+}}
+
